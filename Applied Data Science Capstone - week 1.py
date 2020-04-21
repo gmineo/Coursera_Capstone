7086b699854cf53f46133b3aaf09c30846d8662a
Gmineo <39634149+gmineo@users.noreply.github.com>
2020-04-21 14:46:41 +0200

    Add files via upload

---
+++ Applied Data Science Capstone - week 1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
